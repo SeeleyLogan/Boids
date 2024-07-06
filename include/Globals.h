edc8b970f6ce74587bf618cf6d789bd84a5458c2
Logan Seeley <150400211+SeeleyLogan@users.noreply.github.com>
2024-07-06 00:01:58 -0400

    Changed boids demo video + tweaks
    Tweaked the boids to have more exaggerated alignment, then took a video and changed the video (Video 1) on the github page

--- include/Globals.h
+++ include/Globals.h
@@ -6,10 +6,10 @@
 #define BOIDS 300
 #define BOID_SIZE 1
 #define BOID_SPEED 20
-#define FLOCK_SIZE 40  // Flock size squared
-#define ALIGNMENT_WEIGHT 15
+#define FLOCK_SIZE 20  // Flock size squared
+#define ALIGNMENT_WEIGHT 80
 #define COHESION_WEIGHT 20
-#define SEPARTATION_WEIGHT 20
+#define SEPARTATION_WEIGHT 100
 #define OUT_OF_BOUNDS_WEIGHT 30
 
 #endif
