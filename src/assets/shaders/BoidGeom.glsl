cb4a16a0f62c416b1c8775d02a7d6b44e4618234
Logan Seeley <150400211+SeeleyLogan@users.noreply.github.com>
2024-07-06 15:47:01 -0400

    Changed RGB to HSV
    Boids' colors were based of rotation using RGB, changed to HSV for a nicer effect

--- src/assets/shaders/BoidGeom.glsl
+++ src/assets/shaders/BoidGeom.glsl
@@ -15,6 +15,13 @@ out vec4 ourColor;
 uniform mat4 orangutanProj;
 uniform float boidSize;
 
+vec3 hsv2rgb(vec3 c)
+{
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
 void main()
 {
     vec4 p = gl_in[0].gl_Position;
@@ -30,7 +37,7 @@ void main()
     rotProj[3] = vec4(0.0, 0.0, 0.0, 1.0);
 
     // Create triangle
-    vec4 color = vec4(sin(rot), sin(rot+M_PI/3*2), sin(rot+M_PI/3*4), 1.0);
+    vec4 color = vec4(hsv2rgb(vec3(rot/(2*M_PI), 1, 1)), 1);//vec4(sin(rot), sin(rot+M_PI/3*2), sin(rot+M_PI/3*4), 1.0);
 
     gl_Position = p + vec4(0.0, 1.0*boidSize, 0.0, 0.0) * rotProj * orangutanProj;
     ourColor = color;
